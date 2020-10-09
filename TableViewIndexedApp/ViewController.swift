eef324f678f2ad1ab310e1c60d7e622284f8cdf7
skrall3119 <alexjanci@yahoo.com>
2020-10-09 13:01:32 -0400

    Initial Commit

---
+++ TableViewIndexedApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewIndexedApp
+//
+//  Created by Alex Janci on 10/9/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
